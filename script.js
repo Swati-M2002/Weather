211e1131aa078c1ba74f09f59da1b11bacd60d79
Swati Mandal <mandalswati33@gmail.com>
2024-04-16 10:12:18 +0530

    changes3

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
 
+const API_KEY = "3f2fa6c5c56067d2608761890cbbdfb4"; // API key for OpenWeatherMap API
+
 const getCityCoordinates = () => {
-    const cityName = cityInput.value.trim(); // Get user entered city name and remove extra spaces
-    if (!cityName) {
-        return;
-    }
-    const GEOCODING_API_URL =``;
+    const cityName = cityInput.value.trim(); // Get user entered city name
+    if(!cityName) return; // Return if cityName is empty
+    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+
+    fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+        console.log(data)
+    }).catch(() => {
+        alert("An error occured while fetching the coordinates!");
+    });
 }
-searchButton.addEventListener("click", getCityCoordinates);
